2871974d0185b72fbf9f041c616c046a291ac6bc
Hyun Ahn <codepawn@icloud.com>
2024-03-06 19:21:24 +0900

    updated

--- src/main.js
+++ src/main.js
@@ -1,20 +1,32 @@
 class dddTimer {
   constructor() {
+    // set localStorage
+    if (localStorage.getItem("totalVoteCount") == null) {
+      localStorage.setItem("totalVoteCount", 0);
+    }
+
+    // DOMs
     this.insertFloatdddTimerDiv();
     this.insertDddTimerDiv();
     this.insertDddTimerMillisecondsDiv();
     this.insertDddTimerCountdownDiv();
+    // statics
+    this.insertDddTimerTotalCountDiv();
+    this.insertDddTimerRankDiv();
 
-    this.setCssStyle();
+    this.checkAndUpdateRank();
 
-    setInterval(() => {
-      this.updateBaseTimer();
-    }, 10);
+    // event listeners
+    this.setEventListeners();
+
+    // update
+    this.setIntervalForUpdateBaseTimerMilliseconds(10);
   }
 
   insertFloatdddTimerDiv() {
     let block_to_insert = document.createElement("div");
     block_to_insert.innerHTML = "dddTimer 💙";
+    block_to_insert.classList = "whenSmallScreen";
     block_to_insert.id = "dddTimerFloatDiv";
     document.body.appendChild(block_to_insert);
   }
@@ -47,7 +59,43 @@ class dddTimer {
     document.querySelector("#dddTimerFloatDiv").appendChild(block_to_insert);
   }
 
-  updateBaseTimer() {
+  insertDddTimerTotalCountDiv() {
+    let block_to_insert = document.createElement("span");
+    const total = localStorage.getItem("totalVoteCount");
+    block_to_insert.innerHTML = `총: ${total} `;
+    block_to_insert.id = "dddTimerTotalCountDiv";
+    document.querySelector("#dddTimerFloatDiv").appendChild(block_to_insert);
+  }
+
+  insertDddTimerRankDiv() {
+    let block_to_insert = document.createElement("span");
+    block_to_insert.innerHTML = `🇰🇷🪖`;
+    block_to_insert.id = "dddTimerRankDiv";
+    document.querySelector("#dddTimerFloatDiv").appendChild(block_to_insert);
+  }
+
+  checkAndUpdateRank() {
+    const totalCount = localStorage.getItem("totalVoteCount");
+
+    let rank = "이병";
+    if (5 <= totalCount && totalCount < 20) {
+      rank = "일병";
+    } else if (20 <= totalCount && totalCount < 50) {
+      rank = "상병";
+    } else if (50 <= totalCount && totalCount < 100) {
+      rank = "병장";
+    }
+
+    document.querySelector("#dddTimerRankDiv").innerHTML = `🇰🇷🪖 ${rank}`;
+  }
+
+  setIntervalForUpdateBaseTimerMilliseconds(refreshRate) {
+    setInterval(() => {
+      this.updateBaseTimerMilliseconds();
+    }, refreshRate);
+  }
+
+  updateBaseTimerMilliseconds() {
     this.timerBase = new Date();
 
     document.getElementById("dddTimerDiv").innerHTML =
@@ -59,6 +107,12 @@ class dddTimer {
 
   naverVote() {
     if (!(this.countDonwInterval === undefined)) return;
+
+    let audio = new Audio(chrome.runtime.getURL("./assets/coin_get.mp3"));
+    audio.play();
+
+    // fix scope todo later
+    this.setEventListeners();
 
     this.timeSaved = new Date().getTime();
     let afterTenseconds = this.timeSaved + 10000;
@@ -76,8 +130,17 @@ class dddTimer {
       if (countDown <= 0) {
         document.querySelector("#dddTimerCountdownDiv").innerHTML = "";
 
-        // const audio = new Audio("./assets/clear.mp3");
-        // audio.play();
+        let audio = new Audio(chrome.runtime.getURL("./assets/clear.mp3"));
+        audio.play();
+
+        const addedcount = parseInt(localStorage.getItem("totalVoteCount")) + 1;
+        localStorage.setItem("totalVoteCount", addedcount);
+        const total = localStorage.getItem("totalVoteCount");
+        document.querySelector(
+          "#dddTimerTotalCountDiv"
+        ).innerHTML = `총: ${total} `;
+
+        this.checkAndUpdateRank();
 
         clearInterval(this.countDonwInterval);
         this.countDonwInterval = undefined;
@@ -85,91 +148,55 @@ class dddTimer {
     }, 1000);
   }
 
-  setCssStyle() {
-    // shorthand css inserter
-    const addCSS = (s) =>
-      (document.head.appendChild(document.createElement("style")).innerHTML =
-        s);
+  setEventListeners() {
+    setTimeout(() => {
+      const recommend_selecters = document.querySelectorAll(
+        ".u_cbox_area a.u_cbox_btn_recomm"
+      );
+      const unrecommend_selecters = document.querySelectorAll(
+        ".u_cbox_area a.u_cbox_btn_unrecomm"
+      );
 
-    let centerWidth = window.innerWidth / 2;
-    addCSS(`
-      #dddTimerFloatDiv {
-        position: fixed;
-        left: 649px;
-        top: 93px;
-        text-align: right;
-        background-color: white;
+      let getParentAnchor = (element) => {
+        while (element !== null) {
+          if (element.tagName && element.tagName.toUpperCase() === "A") {
+            return element;
+          }
+          element = element.parentNode;
+        }
+        return null;
+      };
 
-        z-index: 9999;
+      recommend_selecters.forEach((el) =>
+        el.addEventListener(
+          "click",
+          (e) => {
+            let anchor = getParentAnchor(e.target);
+            if (anchor !== null) {
+              dddTimerInstance.naverVote();
+            }
+          },
+          false
+        )
+      );
 
-        border: 1px solid blue;
-        border-radius: 5px;
-
-        padding: 5px;
-      }
-
-      #dddTimerDivMilliseconds {
-        display: inline-block;
-        width: 30px;
-      }
-
-      #dddTimerCountdownDiv {
-        color: blue;
-        font-size: 25px;
-        margin: 5px
-      }
-
-    `);
+      unrecommend_selecters.forEach((el) =>
+        el.addEventListener(
+          "click",
+          (e) => {
+            let anchor = getParentAnchor(e.target);
+            if (anchor !== null) {
+              dddTimerInstance.naverVote();
+            }
+          },
+          false
+        )
+      );
+    }, 1000);
   }
 }
 
+class Rank {}
+
 const dddTimerInstance = new dddTimer();
-
-setTimeout(function () {
-  const recommend_selecters = document.querySelectorAll(
-    ".u_cbox_area a.u_cbox_btn_recomm"
-  );
-  const unrecommend_selecters = document.querySelectorAll(
-    ".u_cbox_area a.u_cbox_btn_unrecomm"
-  );
-
-  var getParentAnchor = function (element) {
-    while (element !== null) {
-      if (element.tagName && element.tagName.toUpperCase() === "A") {
-        return element;
-      }
-      element = element.parentNode;
-    }
-    return null;
-  };
-
-  recommend_selecters.forEach((el) =>
-    el.addEventListener(
-      "click",
-      (e) => {
-        var anchor = getParentAnchor(e.target);
-        if (anchor !== null) {
-          console.log("click");
-          dddTimerInstance.naverVote();
-        }
-      },
-      false
-    )
-  );
-
-  unrecommend_selecters.forEach((el) =>
-    el.addEventListener(
-      "click",
-      (e) => {
-        var anchor = getParentAnchor(e.target);
-        if (anchor !== null) {
-          console.log("click");
-          dddTimerInstance.naverVote();
-        }
-      },
-      false
-    )
-  );
-}, 1000);
-
 console.log("dddtimer loaded");
